34ffcd37c648b511c2988be9dc24f935cf02bb8c
SWETHA-0945 <swethamk45@gmail.com>
2024-06-08 14:01:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-const button = document.querySelector('button');
-const foodItems = [
-  "Chettinad Chicken",
-  "Hyderabadi Biryani",
-  "Malabar Parotta",
-  "Mysore Masala Dosa",
-  "Paneer Tikka Masala",
-  "Butter Chicken"
-];
+document.getElementById('findFoodieFix').addEventListener('click', function() {
+  document.getElementById('flexContainer').style.display = 'flex';
+});
 
-function showFoodItems() {
-  let foodList = "";
-  for (const item of foodItems) {
-    foodList += `<li>${item}</li>`;
-  }
-  const modal = document.createElement('div');
-  modal.classList.add('modal');
-  modal.innerHTML = `<h2>Foodie Delights</h2><ul>${foodList}</ul><button class="close-btn">Close</button>`;
-  document.body.appendChild(modal);
-
-  const closeButton = modal.querySelector('.close-btn');
-  closeButton.addEventListener('click', () => {
-    modal.classList.remove('active'); 
-  });
-  modal.classList.add('active');
-}
-
-button.addEventListener('click', showFoodItems);
 
 let slideIndex = 0;
 showSlides();
